22f290d64a96622adf7f91c87b732a18a582fbff
Constanza <constanza.vp.o@gmail.com>
2024-02-08 13:43:55 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+let nombres = [
+    "Juan",
+    "Luisa",
+    "Fabian",
+    "Jorge",
+    "Elon",
+    "Steve",
+    "Bill",
+    "Estefany"
+    ]
+
+    console.log(nombres.length)
+
+console.log(nombres[5])
+
+const indice = nombres.length-1
+console.log(nombres[indice])
