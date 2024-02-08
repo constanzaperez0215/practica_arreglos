25991794ac1fa227d67f92eb346e5a8cdfeca8d4
Constanza <constanza.vp.o@gmail.com>
2024-02-08 14:02:54 -0300

    ejercicio 2.1 for of

--- app.js
+++ app.js
@@ -15,3 +15,12 @@ console.log(nombres[5])
 
 const indice = nombres.length-1
 console.log(nombres[indice])
+
+// for of
+
+const data = ['Javiera', 'Camila', 'Francisco', 'Jorge', 'Daniela']
+const add = document.getElementById("nombres")
+
+for (const item of data) {
+    add.innerHTML+= '<li>' + item + '</li>'
+}
